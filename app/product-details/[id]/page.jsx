0827fc52caa836178737f0c439c0642559e88efa
nithishp <nithishj03@gmail.com>
2024-08-30 18:07:11 +0530

    deleted additional HERO

--- app/product-details/[id]/page.jsx
+++ app/product-details/[id]/page.jsx
@@ -7,7 +7,7 @@ const ProductDetails = () => {
 
   return (
     <div>
-      <section className="shadow-md body-font overflow-hidden">
+      <section className=" body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
             <Image
